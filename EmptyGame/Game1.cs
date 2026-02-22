e785b19b8972284ecb3c3311cab0c3c2b89e81dc
qorthos <>
2026-02-21 17:43:36 -0700

    stuff

--- EmptyGame/Game1.cs
+++ EmptyGame/Game1.cs
@@ -8,6 +8,9 @@ namespace EmptyGame
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        Texture2D map;
+
+        Vector2 cameraPosition;
 
         public Game1()
         {
@@ -26,7 +29,7 @@ namespace EmptyGame
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            map = Content.Load<Texture2D>("map_hollow");
             // TODO: use this.Content to load your game content here
         }
 
@@ -35,7 +38,29 @@ namespace EmptyGame
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            var ks = Keyboard.GetState();
+            Vector2 input = Vector2.Zero;
+            // wasd movement, 60 pixels per second
+            if (ks.IsKeyDown(Keys.W))
+            {
+                input += new Vector2(0, -1);
+            }
+            if (ks.IsKeyDown(Keys.S))
+            {
+                input += new Vector2(0, 1);
+            }
+            if (ks.IsKeyDown(Keys.A))
+            {
+                input += new Vector2(-1, 0);
+            }
+            if (ks.IsKeyDown(Keys.D))
+            {
+                input += new Vector2(1, 0);
+            }
+
+            cameraPosition += input * 60 * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+
 
             base.Update(gameTime);
         }
@@ -44,7 +69,11 @@ namespace EmptyGame
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            var matrix = Matrix.CreateTranslation(new Vector3(-cameraPosition, 0)) * Matrix.CreateScale(3);
+
+            _spriteBatch.Begin(transformMatrix: matrix, samplerState: SamplerState.PointClamp);
+            _spriteBatch.Draw(map, new Vector2(0, 0), Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
